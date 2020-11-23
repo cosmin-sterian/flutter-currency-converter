363e30669a1aa90f99e817a5f9fd8de75faa69d6
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2020-11-23 18:54:39 +0200

    chore: cleanup code

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Currency converter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,15 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,25 +33,22 @@ class _MyHomePageState extends State<MyHomePage> {
   static const String _ErrorMessage = 'Please use digits and a dot only.';
   static final RegExp _formatRegexp = RegExp(r'^\d+(\.\d*)?$');
 
-  String _currencyString;
   bool _hasError = false;
+  String _inputValue;
   double _currency;
 
-  void _updateCurrencyString(String currencyString) {
-    _currencyString = currencyString;
-    // setState(() {
-    //   _currency = double.parse(currencyString) * _ConversionRate;
-    // });
+  void _updateInputValue(String inputValue) {
+    _inputValue = inputValue;
   }
 
   void _updateCurrency() {
     setState(() {
-      _currency = double.parse(_currencyString) * _ConversionRate;
+      _currency = double.parse(_inputValue) * _ConversionRate;
     });
   }
 
   void _onPressedConvert() {
-    if (_formatRegexp.hasMatch(_currencyString)) {
+    if (_formatRegexp.hasMatch(_inputValue)) {
       _updateCurrency();
       _hasError = false;
     } else {
@@ -86,8 +61,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Column(
@@ -101,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 prefixText: 'EUR',
                 contentPadding: const EdgeInsets.symmetric(horizontal: 10),
                 errorText: _hasError ? _ErrorMessage : null),
-            onChanged: _updateCurrencyString,
+            onChanged: _updateInputValue,
           ),
           if (_currency != null) Text('$_currency RON'),
           TextButton(
